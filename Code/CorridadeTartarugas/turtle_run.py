9681d3fcd539ba366961351fb3491e9a63802a5c
Davi Nakamura Cardoso <davi.nakamura01@gmail.com>
2020-06-25 10:51:53 -0300

    Redigindo o turtle_run.py
    Reescrevendo o código turtle_run.py de modo a deixá-lo mais eficiente e intregrado com a turtle screen.

--- Code/CorridadeTartarugas/turtle_run.py
+++ Code/CorridadeTartarugas/turtle_run.py
@@ -1,24 +1,19 @@
 import random
 import turtle
+import time
 
-Redie = turtle.Turtle()
-Greenie = turtle.Turtle()
-Pinkie = turtle.Turtle()
-Yelly = turtle.Turtle()
-Blu = turtle.Turtle()
 
 screen = turtle.Screen()
 screen.setup(1290, 720)
 screen.bgpic('pavement.gif')
-turtles = [Redie, Blu, Yelly, Pinkie, Greenie]
-names = ['Redie', 'Blu', 'Yelly', 'Pinkie', 'Greenie']
-positions = [-40, -20, 0, 20, 40]
+positions = [-200, -100, 0, 100, 200]
 colors = ['red', 'blue', 'yellow', 'pink', 'white']
-
+turtles = ['', '', '', '', '']
 for i in range(0, 5):
     turtles[i] = turtle.Turtle()
     turtles[i].color(colors[i])
     turtles[i].shape('turtle')
+    turtles[i].turtlesize(5)
     turtles[i].penup()
     turtles[i].setposition(-480, positions[i])
     turtles[i].pendown()
@@ -38,5 +33,19 @@ while not winner:
         if xcor >= 590:
             winner = True
             winner_name = current_turtle.color()
+            time.sleep(1)
+for turtle_i in turtles:
+    if turtle_i.color() != winner_name:
+        turtle_i.hideturtle()
+pencil = turtle.Turtle()
+name = winner_name[0]
+nomes = {'red': 'Vermelha', 'yellow': 'Amarela', 'pink': 'Rosa', 'blue': 'Azul',
+         'white': 'Branca'}
+arg = 'A Tartaruga ' + nomes[name] + ' é a vencedora!'
+pencil.pencolor(name)
+pencil.write(arg, move=False, align="center", font=("Gotham", 45, "normal"))
+pencil.hideturtle()
+screen.exitonclick()
+            
 
-print(winner_name[0], 'é a vencedora')
+
